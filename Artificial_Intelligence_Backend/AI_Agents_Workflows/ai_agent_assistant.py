d2a2c9c3001f245cfa58b4e35e8f53f15ce911a0
oussamaoucouc <oussama.ouq@gmail.com>
2026-01-13 23:34:39 +0100

    still tetsing the rag calculator and excel

--- Artificial_Intelligence_Backend/AI_Agents_Workflows/ai_agent_assistant.py
+++ Artificial_Intelligence_Backend/AI_Agents_Workflows/ai_agent_assistant.py
@@ -15,7 +15,6 @@ from . import model_factory
 from .output_utils import clean_agent_output
 from .ollama_queue import local_llm_rate_limit
 from agno.tools.calculator import CalculatorTools
-from agno.tools.file import FileTools
 
 
 logging.basicConfig(level=logging.INFO)
@@ -77,8 +76,7 @@ async def run_assistant_agent_async(query, user_id, session_id, images=None, aud
     is_tool_unsupported_model = any(pattern in model_id_lower for pattern in TOOL_UNSUPPORTED_MODEL_PATTERNS)
 
     calculator_tools = CalculatorTools(add_instructions=True)
-    # FileTools for reading files
-    file_tools = FileTools()
+
 
     assistant_agent = Agent(
         model=session_model,
@@ -109,7 +107,7 @@ async def run_assistant_agent_async(query, user_id, session_id, images=None, aud
         num_history_responses=5,
         monitoring=True,
         show_tool_calls=False if is_tool_unsupported_model else True,
-        tools=None if is_tool_unsupported_model else [calculator_tools, file_tools],
+        tools=None if is_tool_unsupported_model else [calculator_tools],
         storage = PostgresStorage(table_name="agent_session", db_url=cfg.DB_URL),
         enable_session_summaries=False,
         instructions=dedent("""\
